f727f0c6f90e526f39df4c339d1ff95f77b667d0
Leandro Gonzalez <leanlag@gmail.com>
2025-06-05 12:53:06 -0300

    Agregar modelos de Servicio, Categoría de Servicio e Imagen de Servicio, y actualizar el archivo de inicialización

--- servic/models/__init__.py
+++ servic/models/__init__.py
@@ -1,4 +1,13 @@
 from .user import User, UserRoleChangeLog
 from .provider import ServiceProviderProfile, ProviderRequest
+from .service import ServiceCategory, Service, ServiceImage
 
-__all__ = ["User", "UserRoleChangeLog", "ServiceProviderProfile", "ProviderRequest"]
+__all__ = [
+    "User",
+    "UserRoleChangeLog",
+    "ServiceProviderProfile",
+    "ProviderRequest",
+    "ServiceCategory",
+    "Service",
+    "ServiceImage",
+]
